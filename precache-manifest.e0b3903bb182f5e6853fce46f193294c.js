98ec333d9f71d4ffc74093bcb849d4470a6b5c8f
GigaGeekKPI <life.x.dream.programmer@gmail.com>
2020-08-10 22:35:58 +0300

    Updates

---
+++ precache-manifest.e0b3903bb182f5e6853fce46f193294c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e3674825920a2fd1f0ae4113625167",
+    "url": "/Courses-app/index.html"
+  },
+  {
+    "revision": "99f21571d9d31c9409b5",
+    "url": "/Courses-app/static/css/main.80661ebe.chunk.css"
+  },
+  {
+    "revision": "2016289fc7b5068c5464",
+    "url": "/Courses-app/static/js/2.4d1911cd.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/Courses-app/static/js/2.4d1911cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f21571d9d31c9409b5",
+    "url": "/Courses-app/static/js/main.4af68b9d.chunk.js"
+  },
+  {
+    "revision": "e10ea7c6bc351bd5bbb6",
+    "url": "/Courses-app/static/js/runtime-main.5b93577c.js"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/Courses-app/static/media/delete.650cdc57.svg"
+  },
+  {
+    "revision": "60a471253b51c49c21fd27ed158c6c08",
+    "url": "/Courses-app/static/media/edit.60a47125.svg"
+  },
+  {
+    "revision": "19142cc51b8c074217df3d7499267bf0",
+    "url": "/Courses-app/static/media/search.19142cc5.svg"
+  }
+]);
