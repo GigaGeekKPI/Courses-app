4708b3aeed57aac4360dd0c40e2e392bd137ee7d
GigaGeekKPI <life.x.dream.programmer@gmail.com>
2020-08-12 18:09:21 +0300

    UPDATE: added description to the pet, still needs a lot of fixes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Courses-app
+# Yeah, staying at home, right ?
+![alt text](welcome.jpg "Studying")
+
+This project was bootstrapped with Create React App. Run app -> https://gigageekkpi.github.io/Courses-app/
+
+In the application you can view availible courses and stop procrastinating. Amaizing ! You have:
+
+* Ability to view courses
+* Ability to filter courses by name
+* Ability to edit courses
+* Ability to delete or create new courses.
+
+There's no db, but let's imagine this app is useful. At least you can create your own backend and sell the product. 
+
+Have fun :)
--- src/coursesDB.js
+++ src/coursesDB.js
@@ -1,15 +1,15 @@
 const arr = [
-    {name: 'CSS Optimization', duration: '01h 45min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'ES Next', duration: '02h 20min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'jQuery', duration: '02h', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'Tools', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'React.js', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'React.js + Redux', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'Angular Part 1', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'Angular Part 2', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'JS Patterns + Modules', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'NodeJS / ExpressJS', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'},
-    {name: 'Testing web application', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'Epam'}
+    {name: 'CSS Optimization', duration: '01h 45min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'ES Next', duration: '02h 20min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'jQuery', duration: '02h', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'Tools', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'React.js', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'React.js + Redux', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'Angular Part 1', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'Angular Part 2', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'JS Patterns + Modules', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'NodeJS / ExpressJS', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'},
+    {name: 'Testing web application', duration: '02h 35min', date: '18.08.2020', description: 'Super dooper course', authors: 'RaptorDeveloper'}
 ];
 
 export default arr;
